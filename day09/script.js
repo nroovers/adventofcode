f4d7e8825b2d16f505db4087a04eef1fdfee40e1
nroovers <40662426+nroovers@users.noreply.github.com>
2024-12-09 20:15:46 +0200

    day 9 part 1

---
+++ day09/script.js
@@ -0,0 +1,63 @@
+var fs = require("fs");
+
+var input = fs.readFileSync("./input1.txt", { encoding: "utf8", flag: "r" });
+
+var disk = [];
+
+function addFile(size, id) {
+  for (var i = 0; i < size; i++) {
+    disk.push(id);
+  }
+}
+
+function addEmptySpace(size) {
+  for (var i = 0; i < size; i++) {
+    disk.push(".");
+  }
+}
+
+function intiDisk() {
+  for (var i = 0; i < input.length; i++) {
+    if (i % 2 == 0) {
+      addFile(input[i], Math.floor(i / 2));
+    } else {
+      addEmptySpace(input[i]);
+    }
+  }
+}
+
+function sortDisk() {
+  for (var i = 0; i < disk.length; i++) {
+    if (disk[i] == ".") {
+      var indexOfLastFileBlock = getIndexOfLastFileBlock();
+      if (indexOfLastFileBlock < i) {
+        return;
+      } else {
+        disk[i] = disk[indexOfLastFileBlock];
+        disk[indexOfLastFileBlock] = ".";
+      }
+    }
+  }
+}
+
+function getIndexOfLastFileBlock() {
+  return disk.findLastIndex((block) => block != ".");
+}
+
+intiDisk();
+
+// console.log(disk);
+
+sortDisk();
+
+// console.log(disk);
+
+var checksum = disk
+  .map((value, index) => {
+    return value == "." ? 0 : value * index;
+  })
+  .reduce((total, value) => {
+    return total + value;
+  }, 0);
+
+console.log(checksum);
