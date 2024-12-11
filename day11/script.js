5137eaa51c3e8b8421def16aae5838ef30c4fbc5
nicolai-twoday <nicolai.roovers@twoday.com>
2024-12-11 22:21:12 +0200

    day 11 part 2

--- day11/script.js
+++ day11/script.js
@@ -2,30 +2,45 @@
 var input = "9694820 93 54276 1304 314 664481 0 4";
 
 var array = input.split(" ");
+var arrayLength = 0;
 
-// console.log(array);
+var countCache = {};
 
-function blink() {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] == "0") {
-      array[i] = "1";
-    } else if (array[i].length % 2 == 0) {
-      var left = array[i].slice(0, array[i].length / 2);
-      var right = parseInt(array[i].slice(array[i].length / 2)).toString();
-      array[i] = left;
-      array = array
-        .slice(0, i + 1)
-        .concat([right].concat(array.slice(i + 1, array.length)));
-      i += 1;
-    } else {
-      array[i] = (array[i] * 2024).toString();
-    }
+function countStones(value, i) {
+  var key = `${value}-${i}`;
+  if (countCache[key] != undefined) {
+    return countCache[key];
   }
+  var count = 0;
+  if (i == 0) {
+    count = 1;
+  } else if (value == "0") {
+    count = countStones("1", i - 1);
+  } else if (value.length % 2 == 0) {
+    count =
+      countStones(value.slice(0, value.length / 2), i - 1) +
+      countStones(trimTrailingZero(value.slice(value.length / 2)), i - 1);
+  } else {
+    count = countStones((value * 2024).toString(), i - 1);
+  }
+  countCache[key] = count;
+  return count;
 }
 
-for (var i = 0; i < 25; i++) {
-  blink();
-  console.log(i + 1);
+function trimTrailingZero(str) {
+  return parseInt(str).toString();
 }
 
-console.log("length:", array.length);
+var repeat = 75;
+
+var start = Date.now();
+
+var total = array
+  .map((value) => countStones(value, 75))
+  .reduce((total, value) => total + value, 0);
+
+var end = Date.now();
+
+console.log("time:", (end - start) / 60000);
+
+console.log("stone count:", total);
